91a5e9902985ade7e5e77a85ebe933f6aaaaf7d9
Anubhav Karelia <kareliaanubhav@outlook.com>
2020-04-15 22:02:40 +0530

    Datacamp - Including NameLength Column Using apply method in Python

--- 45_Datacamp_Testing.py
+++ 45_Datacamp_Testing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 brics = pd.read_csv('Datasets/brics.csv', index_col=0)
-for lab, row in brics.iterrows():
-	# Method_01
-	brics.loc[lab,'name_length'] = len(row['country'])
+# for lab, row in brics.iterrows():
+# 	# Method_01
+# 	brics.loc[lab,'name_length'] = len(row['country'])
+brics['name_length'] = brics['country'].apply(len)
 print(brics)
