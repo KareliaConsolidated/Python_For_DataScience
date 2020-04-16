07d565a3ab757d2ba9d6e0218d143ee09f4b3a6f
Anubhav Karelia <kareliaanubhav@outlook.com>
2020-04-16 18:42:38 +0530

    Datacamp Random Walk Visualization

---
+++ 48_Datacamp_Random_Walk_Visulization.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+np.random.seed(123)
+
+# Initialize Random_Walk
+random_walk = [0]
+
+for x in range(100):
+	step = random_walk[-1]
+
+	# Roll the Dice
+	dice = np.random.randint(1,7)
+
+	# Determine Next Step
+	if dice <= 2:
+		step = max(0,step - 1)
+	elif dice in [3,4,5]:
+		step += 1
+	else:
+		step += np.random.randint(1,7)
+
+	# Append Step to Random Walk
+	random_walk.append(step)
+
+print(random_walk)
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Plot random_walk
+plt.plot(random_walk)
+
+# Show the plot
+plt.show()
