25abf3e8f580bd8d4e0c88e7983826ca75fb220e
KareliaConsolidated <kareliaanubhav@outlook.com>
2020-08-01 20:51:13 +0530

    Updated Files

--- 59_DSND/11_Decision_Tree_Information_Gain.py
+++ 59_DSND/11_Decision_Tree_Information_Gain.py
@@ -11,36 +11,35 @@ print(bugs.head())
 # 3   Lobug  Green         23.7
 # 4   Lobug   Blue         18.4
 
-
 def entropy(bugs):
-    lob = bugs[bugs['Species'] == 'Lobug'].shape[0]
-    mob = bugs[bugs['Species'] == 'Mobug'].shape[0]
-    total = bugs['Species'].shape[0]
+  lob = bugs[bugs['Species'] == 'Lobug'].shape[0]
+  mob = bugs[bugs['Species'] == 'Mobug'].shape[0]
+  total = bugs['Species'].shape[0]
 
-    entropy = (-1 * (lob/total) * log((lob/total), 2)) + \
-        (-1 * (mob/total) * log((mob/total), 2))
-    return entropy
-
+  entropy = (-1 * (lob/total) * log((lob/total), 2)) + (-1 * (mob/total) * log((mob/total), 2))
+  return entropy
 
 def information_gain(series):
-    sub_01 = bugs[series].copy()
-    sub_02 = bugs[~series].copy()
+  sub_1 = bugs[series].copy()
+  sub_2 = bugs[~series].copy()
 
-    parent_entropy = entropy(bugs)
+  parent_entropy = entropy(bugs)
 
-    weight_01 = sub_01.shape[0] / bugs.shape[0]
-    weight_02 = sub_02.shape[0] / bugs.shape[0]
+  weight_01 = sub_1.shape[0] / bugs.shape[0]
+  weight_02 = sub_2.shape[0] / bugs.shape[0]
 
-    return parent_entropy - (weight_01 * entropy(sub_01) + weight_02 * entropy(sub_02))
+  entropy_01 = entropy(sub_1)
+  entropy_02 = entropy(sub_2)
 
+  return parent_entropy - (weight_01 * entropy_01 + weight_02 * entropy_02)
 
 for series, string in [(bugs['Color'] == 'Brown', "bugs['Color'] == 'Brown'"),
-                       (bugs['Color'] == 'Blue', "bugs['Color'] == 'Blue'"),
-                       (bugs['Color'] == 'Green', "bugs['Color'] == 'Green'"),
-                       (bugs['Length (mm)'] < 17, "bugs['Length (mm)'] < 17"),
-                       (bugs['Length (mm)'] < 20, "bugs['Length (mm)'] < 20")]:
-    info_gain = information_gain(series)
-    print(f"{string} -> {info_gain:1.4f}")
+               (bugs['Color'] == 'Blue', "bugs['Color'] == 'Blue'"),
+               (bugs['Color'] == 'Green', "bugs['Color'] == 'Green'"),
+               (bugs['Length (mm)'] < 17, "bugs['Color'] < 17"),
+               (bugs['Length (mm)'] < 20, "bugs['Color'] < 20")]:
+  info_gain = information_gain(series)            
+  print(f"{string} :: {info_gain:1.4f}")
 
 # bugs['Color'] == 'Brown' -> 0.0616
 # bugs['Color'] == 'Blue' -> 0.0006
--- 59_DSND/Test.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-data = np.loadtxt('Datasets/MiniBatch.csv', delimiter=',')
-X = data[:,:-1]
-y = data[:, -1]
-print(X.min(), X.max())
